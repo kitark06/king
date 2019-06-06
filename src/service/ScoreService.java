2c5544e3d8980b8004199a4626a9386e02723b77
KartikI <kitark06dev@gmail.com>
2019-06-06 17:46:03 +0530

    mark 5

--- src/service/ScoreService.java
+++ src/service/ScoreService.java
@@ -9,14 +9,20 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ScoreService {
 
     LoginService loginService;
-    ConcurrentHashMap<String,Integer> userHighScore = new ConcurrentHashMap<>();
-    ConcurrentHashMap<String, TreeMap<Integer,User>> highScoreList = new ConcurrentHashMap<>();
+    ConcurrentHashMap<String,Integer> userHighScoreMap = new ConcurrentHashMap<>();
+    ConcurrentHashMap<String, TreeMap<Integer,User>> levelHighScoresMap = new ConcurrentHashMap<>();
 
     public ScoreService(LoginService service) {
         this.loginService = service;
     }
 
     public void postScore(String levelId, String sessionId, int score) {
+
+        // for each level
+        // chmap <lvl,pq>
+        // init pq to cap 15 if null
+        // implement comparable
+        // if score already exists for a user
 
         SessionInfo sessionInfo = loginService.getSessionInfo(sessionId);
 
@@ -26,17 +32,25 @@ public class ScoreService {
 
             //TODO test with zero .. shud not be on output
 
-            int prevScore = userHighScore.get(userId) == null ? 0 : userHighScore.get(userId) ;
+            int prevScore = userHighScoreMap.get(userId) == null ? 0 : userHighScoreMap.get(userId) ;
 
             // check if score is present.. if present, is current greater than prev high score
             if( score > prevScore )
             {
                 // TODO sync
-                // update the map with latest user highscore
-                userHighScore.put(userId, score);
+                // update the map with latest user highscore2
+                userHighScoreMap.put(userId, score);
                 // update level high score & insert into treemap
-                highScoreList.get(levelId).put(score, new User(userId));
+                levelHighScoresMap.get(levelId).put(score, new User(userId));
             }
         }
     }
+
+    /*public String getHighScoreList(String level)
+    {
+        StringBuilder builder = new StringBuilder();
+        levelHighScoresMap.get(level).keySet().forEach(score -> {
+//            System.out.println(score + "="+);
+        });
+    }*/
 }
